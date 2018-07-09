a88e5ac8a17274ed7c924771c902ec7c8d39a28b
Dileep Kishore <k.dileep1994@gmail.com>
2018-07-09 16:56:16 -0400

    Tests for `BiomType`

--- tests/validation/test_otu_validator.py
+++ tests/validation/test_otu_validator.py
@@ -2,10 +2,31 @@
     Module containing tests for the OtuValidator class
 """
 
+from biom import load_table
 import pytest
 from schematics.exceptions import ValidationError
 
-from mindpipe.validation import OtuValidator
+from mindpipe.validation import OtuValidator, BiomType
+
+
+@pytest.mark.usefixtures("biom_data")
+class TestBiomType:
+    """ Tests for the BiomType class """
+
+    def test_init_biom_good(self, biom_data):
+        biom_type = BiomType()
+        for good_biom in biom_data["good"]:
+            assert biom_type.validate(load_table(good_biom))
+
+    def test_init_biom_bad(self, biom_data):
+        biom_type = BiomType()
+        for bad_biom in biom_data["bad"]:
+            if 'empty' in str(bad_biom):
+                with pytest.raises(ValueError):
+                    assert biom_type.validate(load_table(bad_biom))
+            if 'obs_metadata' in str(bad_biom):
+                with pytest.raises(ValidationError):
+                    assert biom_type.validate(load_table(bad_biom))
 
 
 @pytest.mark.usefixtures("biom_data")
@@ -25,6 +46,9 @@ class TestOtuValidator:
         for bad_biom in biom_data["bad"]:
             if 'empty' in str(bad_biom):
                 with pytest.raises(ValueError):
+                    biom_validator.load_validate(bad_biom)
+            if 'obs_metadata' in str(bad_biom):
+                with pytest.raises(ValidationError):
                     biom_validator.load_validate(bad_biom)
             # TODO: Condition for the 'bad.biom'
 
