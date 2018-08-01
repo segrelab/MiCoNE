13227c46cb5e0443a0a62ce88f7d73ba0149e439
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-01 17:39:09 -0400

    Add `tsv_data` fixture to get OTU data in `TSV` format

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,9 +14,22 @@ TEST_DATADIR = BASEDIR / "tests/data"
 @pytest.fixture(scope="module")
 def biom_data():
     """ Fixture that loads biom data """
-    biom_fol = TEST_DATADIR / "biom"
+    biom_fol = TEST_DATADIR / "otus/biom"
     data = {
         "good": list(biom_fol.glob("good/*.biom")),
         "bad": list(biom_fol.glob("bad/*.biom"))
     }
+    return data
+
+@pytest.fixture(scope="module")
+def tsv_data():
+    """ Fixture that loads the tsv data """
+    tsv_fol = TEST_DATADIR / "otus/tsv"
+    data = {"good": [], "bad": []}
+    for kind in ("good", "bad"):
+        for data_fol in (tsv_fol / kind).iterdir():
+            otu = data_fol / "otu.tsv"
+            sample = data_fol / "sample_metadata.tsv"
+            tax = data_fol / "tax_metadata.tsv"
+            data[kind].append((otu, sample, tax))
     return data
