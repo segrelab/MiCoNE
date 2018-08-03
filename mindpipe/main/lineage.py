35fa1471ece47f652e1281e81b75aec3a6728d06
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-03 10:51:02 -0400

    Create `Lineage` a subclass of `BaseLineage`
    The `__new__` method of the `Lineage` class is overridden to ensure that
    the lineage passed in is valid

--- mindpipe/main/lineage.py
+++ mindpipe/main/lineage.py
@@ -11,3 +11,33 @@ BaseLineage = namedtuple(
     "Phylum Class Order Family Genus Species"
 )
 
+
+class Lineage(BaseLineage):
+    """
+        `NamedTuple` that stores the lineage of a taxon and methods to interact with it
+
+        Attributes
+        ----------
+        Phylum: str
+        Class: str
+        Order: str
+        Family: str
+        Genus: str
+        Species: str
+    """
+    def __new__(
+            cls,
+            Phylum: str = '',
+            Class: str = '',
+            Order: str = '',
+            Family: str = '',
+            Genus: str = '',
+            Species: str = ''
+    ) -> "Lineage":
+        tax_order = [Phylum, Class, Order, Family, Genus, Species]
+        empty = [i for i, tax in enumerate(tax_order) if tax == '']
+        if len(tax_order) - empty[0] != len(empty):
+            raise ValueError("Lower levels should not be filled if higher levels are empty")
+        else:
+            return super().__new__(cls, Phylum, Class, Order, Family, Genus, Species)
+
