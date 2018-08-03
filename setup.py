243015d2b4303573abd2dda1d6167cac7ecdeae5
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-03 13:16:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """The setup script."""
 
 from setuptools import setup, find_packages
@@ -11,26 +8,35 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = [
+    'numpy',
+    'pandas',
+    'jinja2',
+    'dask',
+    'cloudpickle',
+    'biom-format',
+    'ete3',
+    'h5py',
+    'statsmodels',
+    'networkx',
+    'toml',
+    'schematics'
+]
 
 setup_requirements = ['pytest-runner', ]
 
-test_requirements = ['pytest', ]
+test_requirements = ['pytest', 'pytest-cov']
 
 setup(
     author="Dileep Kishore",
     author_email='dkishore@bu.edu',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     description="The pipeline that powers the Microbial Interaction Network Database",
     entry_points={
