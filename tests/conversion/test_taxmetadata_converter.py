774f90a2ee890e893472cc4f8784f98ecb3038b2
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-05 11:10:23 -0400

    Add test for 'TAX_CONVERTERS' - `test_qiime2_to_default`

---
+++ tests/conversion/test_taxmetadata_converter.py
@@ -0,0 +1,23 @@
+"""
+    Module containing tests for the taxmetdata_converter
+"""
+
+import pandas as pd
+import pytest
+
+from mindpipe.conversion import TAX_CONVERTERS
+from mindpipe.validation.otu_schema import ObsmetaType
+
+
+@pytest.mark.usefixtures("tax_conversion_data", "tmpdir")
+class TestTaxmetaConverter:
+    """ Tests for the taxmetdata_converter """
+
+    def test_qiime2_to_default(self, tax_conversion_data, tmpdir):
+        in_file = tax_conversion_data["qiime2"]
+        out_file = tmpdir.join("default.csv")
+        converter = TAX_CONVERTERS[("qiime2", "default")]
+        converter(in_file, out_file)
+        obsmeta_type = ObsmetaType()
+        obs_metadata = pd.read_csv(out_file, index_col=0, na_filter=False)
+        obsmeta_type.validate(obs_metadata)
