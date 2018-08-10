ba4a3364c05aa303bf0a6ecd217bd2c0c629e231
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-10 11:00:13 -0400

    Add tests for `Otu.load_data` classmethod

--- tests/main/test_otu.py
+++ tests/main/test_otu.py
@@ -13,5 +13,19 @@ class TestOtu:
 
     def test_init(self, biom_data):
         for biom in biom_data:
-            otu = Otu(biom)
-            assert (otu.otu_data.to_dataframe() == biom.to_dataframe()).any().any()
+            otu_inst = Otu(biom)
+            assert (otu_inst.otu_data.to_dataframe() == biom.to_dataframe()).any().any()
+
+    def test_load_data_biom(self, biom_files):
+        for biom in biom_files["good"]:
+            otu_inst = Otu.load_data(biom)
+            assert hasattr(otu_inst, "otu_data")
+            assert hasattr(otu_inst, "sample_metadata")
+            assert hasattr(otu_inst, "obs_metadata")
+
+    def test_load_data_tsv(self, tsv_files):
+        for otu, sample, tax in tsv_files["good"]:
+            otu_inst = Otu.load_data(otu, sample, tax, dtype="tsv")
+            assert hasattr(otu_inst, "otu_data")
+            assert hasattr(otu_inst, "sample_metadata")
+            assert hasattr(otu_inst, "obs_metadata")
