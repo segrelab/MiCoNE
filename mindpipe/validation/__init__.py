2bb115e09addd4cb474c5f3582d96df2194573e9
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-15 17:13:58 -0400

    Add `InteractionmatrixType` to 'validation' namespace

--- mindpipe/validation/__init__.py
+++ mindpipe/validation/__init__.py
@@ -1,2 +1,3 @@
-from .otu_schema import BiomType, SamplemetaType, ObsmetaType, CorrelationmatrixType, PvaluematrixType
+from .otu_schema import BiomType, SamplemetaType, ObsmetaType
+from .otu_schema import InteractionmatrixType, CorrelationmatrixType, PvaluematrixType
 from .otu_validator import OtuValidator
