04872bf8ac70afb0aa242049d08c8ac3b75dfd67
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-16 17:00:24 -0400

    Add `MetadataType` to 'validation' module namespace

--- mindpipe/validation/__init__.py
+++ mindpipe/validation/__init__.py
@@ -1,3 +1,3 @@
 from .otu_schema import BiomType, SamplemetaType, ObsmetaType
-from .otu_schema import InteractionmatrixType, CorrelationmatrixType, PvaluematrixType
+from .otu_schema import InteractionmatrixType, CorrelationmatrixType, PvaluematrixType, MetadataType
 from .otu_validator import OtuValidator
