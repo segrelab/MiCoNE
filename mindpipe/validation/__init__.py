6dccb3f50e4125289be657cd6f08641c5dbf44d3
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-17 13:29:19 -0400

    Minor style changes

--- mindpipe/validation/__init__.py
+++ mindpipe/validation/__init__.py
@@ -1,4 +1,11 @@
-from .otu_schema import BiomType, SamplemetaType, ObsmetaType
-from .otu_schema import InteractionmatrixType, CorrelationmatrixType, PvaluematrixType
-from .otu_schema import MetadataType, ChildrenmapType
+from .otu_schema import (
+    BiomType,
+    SamplemetaType,
+    ObsmetaType,
+    InteractionmatrixType,
+    CorrelationmatrixType,
+    PvaluematrixType,
+    MetadataType,
+    ChildrenmapType,
+)
 from .otu_validator import OtuValidator
