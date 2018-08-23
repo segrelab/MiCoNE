117749d53c8ede60584d001527a00dfc321fbada
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-23 08:44:58 -0400

    Add tests for `Network.load_data`

--- tests/main/test_network.py
+++ tests/main/test_network.py
@@ -12,19 +12,26 @@ class TestNetwork:
     """ Tests for the Network class """
 
     def test_init(self, correlation_data):
-        cmetadata = {}
-        for corr_data, pval_data, meta_data, child_data, obsmeta_data in correlation_data["good"]:
+        for corr_data, pval_data, meta_data, child_data, obsmeta_data, cmeta_data in correlation_data["good"]:
             assert Network(
                 corr_data,
                 meta_data,
-                cmetadata,
+                cmeta_data,
                 obsmeta_data,
                 pval_data,
                 child_data,
             )
 
     def test_load_data(self, correlation_files):
-        assert True
+        for corr_file, pval_file, meta_file, child_file, obsmeta_file, cmeta_file in correlation_files["good"]:
+            assert Network.load_data(
+                corr_file,
+                meta_file,
+                cmeta_file,
+                obsmeta_file,
+                pval_file,
+                child_file,
+            )
 
     def test_load_from_network(self, network_files):
         assert True
