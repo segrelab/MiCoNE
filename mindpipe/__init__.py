5a1850fa3797fbe8cba49ff6559f486f484917f6
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-23 17:55:12 -0400

    Bump version: 0.2.3 → 0.2.4

--- mindpipe/__init__.py
+++ mindpipe/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Dileep Kishore"""
 __email__ = 'dkishore@bu.edu'
-__version__ = '0.2.3'
+__version__ = '0.2.4'
--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dileep-kishore/mindpipe',
-    version='0.2.3',
+    version='0.2.4',
     zip_safe=False,
 )
