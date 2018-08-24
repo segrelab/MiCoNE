72ba4ccf9d6a341af6f17b343238552f65736d32
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-24 11:25:34 -0400

    Update tests for `Network` class initialization

--- tests/main/test_network.py
+++ tests/main/test_network.py
@@ -2,8 +2,10 @@
     Module containing tests for the Network class
 """
 
-import pytest
+import json
 
+import pytest
+import pandas as pd
 from mindpipe.main import Network
 
 
@@ -13,7 +15,7 @@ class TestNetwork:
 
     def test_init(self, correlation_data):
         for corr_data, pval_data, meta_data, child_data, obsmeta_data, cmeta_data in correlation_data["good"]:
-            assert Network(
+            network = Network(
                 corr_data,
                 meta_data,
                 cmeta_data,
@@ -21,10 +23,15 @@ class TestNetwork:
                 pval_data,
                 child_data,
             )
+            n_nodes = corr_data.shape[0]
+            assert n_nodes == len(network.nodes)
+            assert (n_nodes ** 2 - n_nodes) // 2 == len(network.links)
+            assert len(network.links_thres) <= len(network.links)
+            assert all(key in network.metadata for key in meta_data)
 
     def test_load_data(self, correlation_files):
         for corr_file, pval_file, meta_file, child_file, obsmeta_file, cmeta_file in correlation_files["good"]:
-            assert Network.load_data(
+            network = Network.load_data(
                 corr_file,
                 meta_file,
                 cmeta_file,
@@ -32,6 +39,14 @@ class TestNetwork:
                 pval_file,
                 child_file,
             )
+            corr_data = pd.read_table(corr_file, index_col=0)
+            with open(meta_file, 'r') as fid:
+                meta_data = json.load(fid)
+            n_nodes = corr_data.shape[0]
+            assert n_nodes == len(network.nodes)
+            assert (n_nodes ** 2 - n_nodes) // 2 == len(network.links)
+            assert len(network.links_thres) <= len(network.links)
+            assert all(key in network.metadata for key in meta_data)
 
     def test_load_from_network(self, network_files):
         assert True
