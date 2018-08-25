30ef4631260c12836b2d1268d9c9360d10c50b0c
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-25 12:10:46 -0400

    Implement `NETWORK_CONVERTERS` to convert networks to various formats
    Current only `json_to_elist` converter is implemented

--- mindpipe/conversion/__init__.py
+++ mindpipe/conversion/__init__.py
@@ -1,1 +1,2 @@
 from .taxmetadata_converter import CONVERTERS as TAX_CONVERTERS
+from .network_converter import CONVERTERS as NETWORK_CONVERTERS
--- mindpipe/conversion/network_converter.py
+++ mindpipe/conversion/network_converter.py
@@ -2,5 +2,34 @@
     Module containing methods that convert networks into various formats
 """
 
-# TODO: Edge list to interaction matrix
-# TODO: interaction matrix to edge list
+import pathlib
+
+import pandas as pd
+
+from ..main import Network
+
+
+def json_to_elist(in_file: pathlib.Path, out_file: pathlib.Path) -> None:
+    """
+        Convert Network file from json to elist format
+        Note that only the edge attributes can be converted
+
+        Parameters
+        ----------
+        in_file : pathlib.Path
+            The path to the json formatted network file
+        out_file : pathlib.Path
+            The path to the elist formatted network file
+    """
+    network = Network.load_json(in_file)
+    df = pd.DataFrame.from_dict(network.links)
+    cols = list(df.columns)
+    cols.remove("source")
+    cols.remove("target")
+    df = df[["source", "target"] + cols]
+    df.to_csv(out_file, index=False)
+
+
+CONVERTERS = {
+    ("json", "elist"): json_to_elist,
+}
