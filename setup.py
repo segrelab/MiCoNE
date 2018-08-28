f835ff02ff195b39501813ed3295c174133b5450
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-28 13:43:18 -0400

    Bump version: 0.2.4 → 0.3.0

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dileep-kishore/mindpipe',
-    version='0.2.4',
+    version='0.3.0',
     zip_safe=False,
 )
