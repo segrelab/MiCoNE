57b487c6027171a2d450d073c2f5bfbeb5d71024
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-30 21:45:47 -0400

    Initial `pipeline_process.py` stub

---
+++ mindpipe/pipelines/pipeline_process.py
@@ -0,0 +1,10 @@
+"""
+    Module that defines different pipeline processes
+"""
+
+# TODO
+# BaseProcess
+# NFProcess(BaseProcess)
+# InternalProcess(BaseProcess)
+# FormatterProcess(BaseProcess)
+# HelperProcess(BaseProcess)
