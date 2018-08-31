6aada475ee9cb93c62de3aa8b3ec1079d1074a29
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-30 21:43:07 -0400

    Initial `settings.py` stub

---
+++ mindpipe/config/settings.py
@@ -0,0 +1,8 @@
+"""
+    Module to extract, process and store pipeline settings
+"""
+
+# TODO:
+# init Config
+# parse_settings
+# How to handle different types of process types - nf, internal, formatters
