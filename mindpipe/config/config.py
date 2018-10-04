c52e8a0d493006d1b799693f42fdefcfbc7422d1
Dileep Kishore <k.dileep1994@gmail.com>
2018-10-04 11:30:47 -0400

    Implement `Config` class. Fixes #53
    The `Config` class processes and stores the pipeline settings

---
+++ mindpipe/config/config.py
@@ -0,0 +1,86 @@
+"""
+    Module to extract, process and store pipeline settings
+"""
+
+import pathlib
+from typing import Union
+
+import toml
+
+from .datatypes import DataTypes
+from .process_params import InternalProcessParamsSet, ExternalProcessParamsSet
+
+
+DATATYPES_FILE = pathlib.Path.cwd() / "datatypes.toml"
+EXTERNAL_FILE = pathlib.Path.cwd() / "external.toml"
+INTERNAL_FILE = pathlib.Path.cwd() / "internal.toml"
+ParamType = Union[InternalProcessParamsSet, ExternalProcessParamsSet]
+
+
+class Config:
+    """
+        Class to extract, process and store pipeline settings
+
+        Parameters
+        ----------
+        datatypes_file : pathlib.Path
+            The path to the datatypes definition file
+        external_file : pathlib.Path
+            The path to the external process defintion file
+        internal_file : pathlib.Path
+            The path to the internal process defintion file
+
+        Attributes
+        ----------
+        datatypes : DataTypes
+            The set of supported datatypes
+        process_params : Dict[str, ParamType]
+            The dictionary containing internal and external process configurations
+    """
+
+    def __init__(
+            self,
+            datatypes_file: pathlib.Path = DATATYPES_FILE,
+            external_file: pathlib.Path = EXTERNAL_FILE,
+            internal_file: pathlib.Path = INTERNAL_FILE,
+    ) -> None:
+        with open(datatypes_file, 'r') as fid:
+            datatypes = DataTypes(toml.load(fid))
+        self.datatypes = datatypes
+        with open(external_file, 'r') as fid:
+            external_params = ExternalProcessParamsSet(toml.load(fid))
+        self._check_io_integrity(external_params)
+        with open(internal_file, 'r') as fid:
+            internal_params = InternalProcessParamsSet(toml.load(fid))
+        self._check_io_integrity(internal_params)
+        self.process_params = {
+            "internal": internal_params,
+            "external": external_params,
+        }
+
+    def _check_io_integrity(self, process_params: ParamType) -> None:
+        """
+            Verify whether all the datatypes in the process_params are valid
+
+            Parameters
+            ----------
+            process_params : ParamType
+                The params to be verified
+        """
+        for param in process_params:
+            for curr_input in param.input:
+                if curr_input.datatype not in self.datatypes:
+                    raise ValueError(f"Invalid datatype {curr_input.datatype} in input definition")
+                formats = self.datatypes[curr_input.datatype].format
+                for curr_format in curr_input.format:
+                    if curr_format not in formats:
+                        raise ValueError(f"Unsupported format {curr_format} in input definition")
+            for curr_output in param.output:
+                if curr_output.datatype not in self.datatypes:
+                    raise ValueError(f"Invalid datatype {curr_output.datatype} in output definition")
+                formats = self.datatypes[curr_output.datatype].format
+                for curr_format in curr_output.format:
+                    if curr_format not in formats:
+                        raise ValueError(f"Unsupported format {curr_format} in output definition")
+                if not curr_output.location:
+                    raise ValueError(f"Relative location for output {curr_output} must be defined")
--- mindpipe/config/settings.py
+++
@@ -1,8 +0,0 @@
-"""
-    Module to extract, process and store pipeline settings
-"""
-
-# TODO:
-# init Config
-# parse_settings
-# How to handle different types of process types - nf, internal, formatters
