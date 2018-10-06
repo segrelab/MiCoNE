93c52ca6318d2661b7b72650dd0f35dc3ab8e2ad
Dileep Kishore <k.dileep1994@gmail.com>
2018-10-06 12:09:26 -0400

    Add tests for `ProcessParamsSet` and `ProcessParams` class

---
+++ tests/config/test_process_params.py
@@ -0,0 +1,69 @@
+"""
+    Module containing tests for the ProcessParams class
+"""
+
+import pytest
+
+from mindpipe.config import InternalProcessParamsSet, ExternalProcessParamsSet
+from mindpipe.config.process_params import ProcessParams
+
+
+@pytest.mark.usefixtures("pipeline_settings")
+class TestProcessParamsSet:
+    """ Tests for ProcessParamsSet class """
+
+    def test_init(self, pipeline_settings):
+        internal_raw = pipeline_settings["internal"]
+        external_raw = pipeline_settings["external"]
+        assert InternalProcessParamsSet(internal_raw)
+        assert ExternalProcessParamsSet(external_raw)
+        wrong_format = {
+            'root': 'src/internal/split_otu_table',
+            'output_location': 'split_otu_table',
+            'input': [{'datatype': 'sequence_16s', 'format': ['fasta']}],
+            'output': [{'datatype': 'otu_table', 'format': ['biom'], 'location': '*.biom'}],
+            'parameters': [{'process': 'something', 'data': 123}],
+        }
+        assert ProcessParams(("wrong_format", wrong_format))
+        with pytest.raises(TypeError):
+            ProcessParams(("wrong_format", {**wrong_format, "input": "string"}))
+        with pytest.raises(TypeError):
+            ProcessParams(("wrong_format", {**wrong_format, "output": "string"}))
+        with pytest.raises(TypeError):
+            ProcessParams(("wrong_format", {**wrong_format, "parameters": "string"}))
+        with pytest.raises(ValueError):
+            ProcessParams(("wrong_format", {**wrong_format, "input": [{'datatype': 'sequence_16s'}]}))
+        with pytest.raises(ValueError):
+            ProcessParams(("wrong_format", {**wrong_format, "output": [{'datatype': 'sequence_16s'}]}))
+        with pytest.raises(ValueError):
+            ProcessParams(("wrong_format", {**wrong_format, "parameters": [{'data': 'temp'}]}))
+
+    def test_iter_len(self, pipeline_settings):
+        internal_raw = pipeline_settings["internal"]
+        internal = InternalProcessParamsSet(internal_raw)
+        assert len(internal_raw) == len(internal)
+        for process in internal:
+            assert isinstance(process, ProcessParams)
+        external_raw = pipeline_settings["external"]
+        external = ExternalProcessParamsSet(external_raw)
+        count = 0
+        for l1 in external_raw:
+            for l2 in external_raw[l1]:
+                for l3 in external_raw[l1][l2]:
+                    count += 1
+        assert count == len(external)
+        for process in external:
+            assert isinstance(process, ProcessParams)
+
+    def test_contains_getitem(self, pipeline_settings):
+        internal_raw = pipeline_settings["internal"]
+        external_raw = pipeline_settings["external"]
+        internal = InternalProcessParamsSet(internal_raw)
+        external = ExternalProcessParamsSet(external_raw)
+        test_internal_key = list(internal_raw.keys())[0]
+        for l1 in external_raw:
+            for l2 in external_raw[l1]:
+                for l3 in external_raw[l1][l2]:
+                    test_external_key = f"{l1}.{l2}.{l3}"
+                    assert test_external_key in external
+        assert test_internal_key in internal
