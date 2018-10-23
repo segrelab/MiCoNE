47f1e5f96f4edb615fd9fb48937eab564915bc1e
Dileep Kishore <k.dileep1994@gmail.com>
2018-10-23 13:55:41 -0400

    Implement `Pipeline` class (just initialization)
    This class defines the pipeline and contains method to run the pipeline.
    It has the following attributes:
    1. `title` - title of the pipeline
    2. `output_location` - the location where the entire pipeline output is
       stored
    3. `processes` - the list of processes in the pipeline in order

--- mindpipe/pipelines/pipeline.py
+++ mindpipe/pipelines/pipeline.py
@@ -2,7 +2,109 @@
     Module that defines a complete pipeline by incorporating settings and processes
 """
 
-# TODO
-# init Pipeline
-# read_settings
-# create_processes
+import collections
+from typing import Iterator, List, Union
+
+import toml
+
+from ..config import Config
+from .process import InternalProcess, ExternalProcess
+
+
+class Pipeline(collections.Sequence):
+    """
+        Class that defines the pipeline and contains methods to run the pipeline
+
+        Parameters
+        ----------
+        user_settings_file : str
+            The user created settings file that describes the pipeline
+
+        Attributes
+        ----------
+        title : str
+            The title of the pipeline
+        output_location : str
+            The base output location to store all pipeline results
+        processes : List[Union[InternalProcess, ExternalProcess]]
+            The list of `Process` in the pipeline
+    """
+    _req_keys = {
+        "title",
+        "order"
+        "output_location",
+    }
+
+    def __init__(self, user_settings_file: str) -> None:
+        self.config = Config()
+        user_settings = self._parse_settings(user_settings_file)
+        self.title = user_settings["title"]
+        self.output_location = user_settings["output_location"]
+        self.processes = self._create_processes(user_settings)
+
+    def _parse_settings(self, settings_file: str) -> dict:
+        """
+            Parses the user created settings file
+
+            Parameters
+            ----------
+            settings_file : str
+                The user defined settings file that describes the pipeline
+
+            Returns
+            -------
+            dict
+                The dictionary of verified user settings
+        """
+        with open(settings_file, 'r') as fid:
+            settings = toml.load(fid)
+        for key in self._req_keys:
+            if key not in settings:
+                raise ValueError(f"Required key {key} not in user_settings")
+        return settings
+
+    def _create_processes(self, settings):
+        process_list: List[Union[InternalProcess, ExternalProcess]] = []
+        process_namelist = settings["order"]
+        for process_name in process_namelist:
+            if '.' in process_name:
+                temp = settings
+                for x in process_name.split('.'):
+                    temp = temp[x]
+                user_process_data = temp
+            else:
+                user_process_data = settings[process_name]
+            process_data = self.config.process_params[user_process_data["module"]][process_name]
+            process_data.merge(user_process_data)
+            if user_process_data["module"] == "internal":
+                process_list.append(InternalProcess(process_data))
+            elif process_data["module"] == "external":
+                process_list.append(ExternalProcess(process_data))
+            else:
+                raise ValueError(f"Unsupported process type: {process_data['module']}")
+        # TODO: Connect output to inputs (check if order is important)
+        for current, previous in zip(process_list[:-1], process_list[1:]):
+            current.attach_to(previous)
+            current.update_location(self.output_location)
+        return process_list
+
+    def __iter__(self) -> Iterator:
+        return iter(self.processes)
+
+    def __len__(self) -> int:
+        return len(self.processes)
+
+    def __getitem__(self, key: str) -> Union[InternalProcess, ExternalProcess]:
+        for process in self:
+            if process.name == key:
+                return process
+        raise KeyError(f"{key} is not a process of this pipeline")
+
+    def __repr__(self) -> str:
+        return (
+            f"<Pipeline title={self.title} output_location={self.output_location} "
+            f"processes={[p.name for p in self]}>"
+        )
+
+    def __str__(self) -> str:
+        return self.title
