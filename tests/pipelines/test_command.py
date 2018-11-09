5949dee3223d544f0f814692d4c9c33a26399ff3
Dileep Kishore <k.dileep1994@gmail.com>
2018-11-09 15:42:47 -0500

    Test: Add tests for the `Command` class

---
+++ tests/pipelines/test_command.py
@@ -0,0 +1,61 @@
+"""
+    Module containing tests for the `Command` class
+"""
+
+import os
+
+import pytest
+
+from mindpipe.pipelines import Command
+
+
+# TODO: How can you test 'sge'? Just checking the commands?
+@pytest.mark.usefixtures("example_pipeline")
+class TestCommand:
+    """ Tests for the `Command` class """
+
+    def test_init(self):
+        cmd = "ls"
+        profile = "local"
+        timeout = 1000
+        command = Command(cmd, profile, timeout)
+        assert command.run()
+
+    def test_output(self):
+        cmd = "uname"
+        profile = "local"
+        timeout = 1000
+        command = Command(cmd, profile, timeout)
+        with pytest.raises(NotImplementedError):
+            command.output
+        command.run()
+        output = command.output
+        assert output == "Linux\n"
+
+    def test_error(self):
+        cmd = "random_command"
+        profile = "local"
+        timeout = 1000
+        command = Command(cmd, profile, timeout)
+        command.run()
+        error = command.error
+        print(error)
+        assert error == "/bin/sh: random_command: command not found\n"
+
+    def test_log(self, tmpdir):
+        cmd = "ls"
+        profile = "local"
+        timeout = 1000
+        command = Command(cmd, profile, timeout)
+        command.run()
+        log_file = tmpdir.mkdir("test_command_log").join("log.txt")
+        assert not os.path.exists(log_file)
+        command.log(log_file)
+        assert os.path.exists(log_file)
+
+    def test_profile(self):
+        cmd = "ls"
+        profile = "sge"
+        timeout = 1000
+        command = Command(cmd, profile, timeout)
+        assert command.cmd.startswith("qsub")
