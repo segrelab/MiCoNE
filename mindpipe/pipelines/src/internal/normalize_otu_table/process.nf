4edb2278d38c22073b83ca7ce28964e35351355d
Dileep Kishore <k.dileep1994@gmail.com>
2018-11-27 12:16:28 -0500

    Fix: normalize_otu_table pipeline working now

--- mindpipe/pipelines/src/internal/normalize_otu_table/process.nf
+++ mindpipe/pipelines/src/internal/normalize_otu_table/process.nf
@@ -9,8 +9,6 @@ def prevalence_thres = params.prevalence_thres
 def abundance_thres = params.abundance_thres
 def rm_sparse_obs = params.rm_sparse_obs
 def rm_sparse_samples = params.rm_sparse_samples
-def rm_sparse_obs = params.rm_sparse_obs
-def rm_sparse_samples = params.rm_sparse_samples
 
 Channel
     .fromPath(otudata)
@@ -20,7 +18,7 @@ Channel
 
 process normalize_otu_table {
     tag "$id"
-    publishDir "${output_dir}/normalize_otu_table/${id}"
+    publishDir "${output_dir}/normalize_otu_table"
 
     input:
     set val(id), file(otu_file) from otudata_chnl
--- mindpipe/pipelines/src/internal/normalize_otu_table/processes/normalize_otu_table.py
+++ mindpipe/pipelines/src/internal/normalize_otu_table/processes/normalize_otu_table.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 
-from minpipe import Otu
+from mindpipe import Otu
 
 
-def main(otu_file: str, rm_sparse_samples: bool, rm_sparse_obs: bool) -> Otu:
+def main(
+    otu_file: str,
+    rm_sparse_samples: bool,
+    rm_sparse_obs: bool,
+    axis: str,
+    count_thres: int,
+    prevalence_thres: float,
+    abundance_thres: float,
+) -> Otu:
     otu = Otu.load_data(otu_file)
     if rm_sparse_samples:
-        otu = otu.rm_sparse_samples()
+        otu = otu.rm_sparse_samples(count_thres=count_thres)
     if rm_sparse_obs:
-        otu = otu.rm_sparse_obs()
-    return otu.normalize()
+        otu = otu.rm_sparse_obs(
+            prevalence_thres=prevalence_thres, abundance_thres=abundance_thres
+        )
+    return otu.normalize(axis=axis)
 
 
 if __name__ == "__main__":
@@ -29,4 +39,4 @@ if __name__ == "__main__":
         PREVALENCE_THRES,
         ABUNDANCE_THRES,
     )
-    norm_otu.write("norm_otu.biom")
+    norm_otu.write("$id" + "_normalized")
