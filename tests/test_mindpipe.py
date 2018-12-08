a4b1d150c203ca5825d4d7bfd048f0d5434afb25
Dileep Kishore <k.dileep1994@gmail.com>
2018-12-08 13:53:39 -0500

    Tests: Fix CLI tests

--- tests/test_mindpipe.py
+++ tests/test_mindpipe.py
@@ -10,28 +10,12 @@ from mindpipe import mindpipe
 from mindpipe import cli
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
+    result = runner.invoke(cli.cli)
     assert result.exit_code == 0
-    assert "mindpipe.cli.main" in result.output
-    help_result = runner.invoke(cli.main, ["--help"])
+    assert "Main entry point to mindpipe" in result.output
+    help_result = runner.invoke(cli.cli, ["--help"])
     assert help_result.exit_code == 0
-    assert "--help  Show this message and exit." in help_result.output
+    assert "Show this message and exit." in help_result.output
