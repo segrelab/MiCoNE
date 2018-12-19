ed6ae1d7542a2195cf8f63ad4e7ac0cada8df312
Dileep Kishore <k.dileep1994@gmail.com>
2018-12-19 08:56:30 -0500

    Add: A wrapper class around `loguru.logger`

--- mindpipe/logging/logger.py
+++ mindpipe/logging/logger.py
@@ -10,18 +10,71 @@ from loguru import logger
 
 TMP_FOL = pathlib.Path("/tmp/mindpipe")
 TMP_FOL.mkdir(exist_ok=True)
-FNAME = "".join(random.choices(string.ascii_lowercase + string.digits, k=16)) + ".log"
-TMP_FILE = TMP_FOL / FNAME
 
-CONFIG = {
-    "handlers": [
-        {
-            "sink": str(TMP_FILE),
-            "format": "{time:YY-MM-DD at HH:mm:ss} | {level} | {message}",
-        }
-    ]
-}
 
-logger.configure(**CONFIG)
-logger.disable("mindpipe")
-LOG = logger
+class Log:
+    """
+        Class that handles the logging
+        Wrapper around `loguru.logger`
+
+        Parameters
+        ----------
+        folder : pathlib.Path
+            Path to the folder containing the log files
+            The folder must already exist
+        format_str : str
+            Format string for the log file
+            "timestamp | level | message"
+        disable : bool
+            Flag to disable logging at the start
+            Default value is True
+
+        Attributes
+        ----------
+        path : pathlib.Path
+            The path to the log file
+        config : dict
+            The dictionary used to configure the `loguru.logger`
+        logger : loguru.logger
+            The main logging object
+    """
+
+    def __init__(
+        self, folder: pathlib.Path, format_str: str, disable: bool = True
+    ) -> None:
+        fname = (
+            "".join(random.choices(string.ascii_lowercase + string.digits, k=16))
+            + ".log"
+        )
+        self._folder = folder
+        self.path = folder / fname
+        self.config = {"handlers": [{"sink": str(self.path), "format": format_str}]}
+        logger.configure(**self.config)
+        self.logger = logger
+        if disable:
+            self.logger.disable("mindpipe")
+
+    def enable(self) -> None:
+        """ Enable logging """
+        self.logger.enable("mindpipe")
+
+    def cleanup(self, nfiles: int = 10) -> None:
+        """
+            Delete old logs if number > nfiles
+
+            Parameters
+            ----------
+            nfiles : int
+                The threshold for number of log files beyond which the older ones are deleted
+                Default value is 10
+        """
+        log_files = list(self._folder.glob("*.log"))
+        log_files_srtd = sorted(
+            log_files, key=lambda x: x.stat().st_mtime, reverse=True
+        )
+        log_files_remove = log_files_srtd[nfiles:]
+        for file in log_files_remove:
+            file.unlink()
+
+
+LOG = Log(TMP_FOL, format_str="{time:YY-MM-DD at HH:mm:ss} | {level} | {message}")
