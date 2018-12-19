1d89fee6f527015f03c356508f27a5991ced5c3e
Dileep Kishore <k.dileep1994@gmail.com>
2018-12-19 08:10:29 -0500

    Change: Create log files in `/tmp` folder. Fixes #106

--- mindpipe/logging/logger.py
+++ mindpipe/logging/logger.py
@@ -2,12 +2,21 @@
     Module that contains the logger configuration
 """
 
+import pathlib
+import random
+import string
+
 from loguru import logger
+
+TMP_FOL = pathlib.Path("/tmp/mindpipe")
+TMP_FOL.mkdir(exist_ok=True)
+FNAME = "".join(random.choices(string.ascii_lowercase + string.digits, k=16)) + ".log"
+TMP_FILE = TMP_FOL / FNAME
 
 CONFIG = {
     "handlers": [
         {
-            "sink": "mindpipe.log",
+            "sink": str(TMP_FILE),
             "format": "{time:YY-MM-DD at HH:mm:ss} | {level} | {message}",
         }
     ]
