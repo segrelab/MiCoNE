5d24100f2dbce7a356ea4121ca4a7747eeace157
Dileep Kishore <k.dileep1994@gmail.com>
2019-01-12 15:34:07 -0500

    Refactor: trimming process of trim_filter_fixed pipeline

--- mindpipe/pipelines/src/otu_assignment/sequence_processing/trim_filter_fixed/processes/trimming.R
+++ mindpipe/pipelines/src/otu_assignment/sequence_processing/trim_filter_fixed/processes/trimming.R
@@ -5,6 +5,7 @@ suppressWarnings(library(dada2))
 multithread <- ${ncpus}
 maxEE <- ${max_ee}
 truncQ <- ${trunc_q}
+trim_cmd <- "${trim_cmd}"
 
 untrimmed.reads <- list.files(".", pattern=".fastq.gz\$", full.names=TRUE)
 
@@ -13,15 +14,27 @@ untrimmed.reads <- list.files(".", pattern=".fastq.gz\$", full.names=TRUE)
   }
 
 # read trim and filter commands
-trim.cmd <- read.csv("${trim_cmd}", header=FALSE)
+trim.cmd <- read.csv(trim_cmd, header=FALSE)
 trimLeft <- trim.cmd[1,2]
 truncLen <- trim.cmd[2,2]
 
 
 # trim sequences
 trimmed.reads <- file.path("trimmed", basename(untrimmed.reads))
-out <- suppressWarnings(filterAndTrim(untrimmed.reads, trimmed.reads, truncLen=truncLen, trimLeft=trimLeft,
-                                      maxEE=maxEE, truncQ=truncQ, rm.phix=TRUE, multithread=multithread))
+out <- suppressWarnings(
+    filterAndTrim(
+        untrimmed.reads,
+        trimmed.reads,
+        truncLen=truncLen,
+        trimLeft=trimLeft,
+        maxEE=maxEE,
+        truncQ=truncQ,
+        rm.phix=TRUE,
+        multithread=multithread
+    )
+)
+
+# check trimmed reads
 cat(ifelse(file.exists(trimmed.reads), ".", "x"), sep="")
 trimmed.reads <- list.files("trimmed", pattern=".fastq.gz\$", full.names=TRUE)
 cat("\n")
