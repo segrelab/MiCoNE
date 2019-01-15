d8360ebd52a93b288de89198b326abd6ce53bb34
Dileep Kishore <k.dileep1994@gmail.com>
2019-01-15 16:04:22 -0500

    Fix: Incorrect hashing in `dada2` hashing process
    We were hashing the `seq.id` instead of `seq.seq` :D
    Also added assertions to make sure that this doesn't repeat

--- mindpipe/pipelines/src/otu_assignment/denoise_cluster/dada2/processes/hashing.py
+++ mindpipe/pipelines/src/otu_assignment/denoise_cluster/dada2/processes/hashing.py
@@ -15,26 +15,32 @@ def hash_function(seq):
 def hash_otu_table(unhashed_otu_table, output_file):
     table = load_table(unhashed_otu_table)
     df = table.to_dataframe(dense=True)
-    df.index = list(map(hash_function, df.index))
+    seq_ids = list(map(hash_function, df.index))
+    df.index = seq_ids
     new_table = Table(df.values, list(df.index), list(df.columns))
     with biom_open(output_file, "w") as fid:
         new_table.to_hdf5(fid, "Constructucted by mindpipe in dada2 pipeline")
+    return seq_ids
 
 
 def hash_rep_seqs(unhashed_rep_seqs, output_file):
     seqs = list(SeqIO.parse(unhashed_rep_seqs, "fasta"))
+    seq_ids = []
     for seq in seqs:
-        seq.id = hash_function(seq.id)
+        seq.id = hash_function(str(seq.seq))
+        seq_ids.append(seq.id)
         seq.description = ""
         seq.name = ""
     with open(output_file, "w") as fid:
         fasta_writer = FastaIO.FastaWriter(fid, wrap=None)
         fasta_writer.write_file(seqs)
+    return seq_ids
 
 
 def hashing(unhashed_otu_table, unhashed_rep_seqs):
-    hash_otu_table(unhashed_otu_table, "otu_table.biom")
-    hash_rep_seqs(unhashed_rep_seqs, "rep_seqs.fasta")
+    otu_table_ids = hash_otu_table(unhashed_otu_table, "otu_table.biom")
+    rep_seq_ids = hash_rep_seqs(unhashed_rep_seqs, "rep_seqs.fasta")
+    assert otu_table_ids == rep_seq_ids
 
 
 if __name__ == "__main__":
