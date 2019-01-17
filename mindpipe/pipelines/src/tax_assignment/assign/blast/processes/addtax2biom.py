fdf0bb5eccbfbf29f282227a5ea587ab57d7601d
Dileep Kishore <k.dileep1994@gmail.com>
2019-01-17 13:51:48 -0500

    Fix: Remove prefixes from taxonomy names in `blast` pipeline
    Prefixes are `s__`

--- mindpipe/pipelines/src/tax_assignment/assign/blast/processes/addtax2biom.py
+++ mindpipe/pipelines/src/tax_assignment/assign/blast/processes/addtax2biom.py
@@ -11,7 +11,7 @@ HEADERS = ["Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"]
 
 
 def tax_splitter(full_tax):
-    tax_list = [tax.strip() for tax in full_tax.Taxon.split(";")]
+    tax_list = [tax.strip().split("__")[-1] for tax in full_tax.Taxon.split(";")]
     tax_list.extend([""] * (len(HEADERS) - len(tax_list)))
     return pd.Series(data=tax_list, index=HEADERS)
 
