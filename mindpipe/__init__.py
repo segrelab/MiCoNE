ca5f85b6e6dfe4ab45a2e1657063f1cd6bfbddd6
Dileep Kishore <k.dileep1994@gmail.com>
2019-01-17 15:54:13 -0500

    Add: `NetworkGroup` to main namespace

--- mindpipe/__init__.py
+++ mindpipe/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Dileep Kishore"""
 __email__ = "dkishore@bu.edu"
 __version__ = "0.5.0"
 
-from .main import Lineage, Network, Otu
+from .main import Lineage, Network, NetworkGroup, Otu
--- mindpipe/main/__init__.py
+++ mindpipe/main/__init__.py
@@ -1,3 +1,4 @@
 from .otu import Otu
 from .lineage import Lineage
 from .network import Network
+from .network_group import NetworkGroup
