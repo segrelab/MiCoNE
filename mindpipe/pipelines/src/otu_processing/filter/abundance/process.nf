6d60f077af77a9ab980d676d0a4864c034272cd3
Dileep Kishore <k.dileep1994@gmail.com>
2019-01-18 14:33:22 -0500

    Add: otu_processing.filter.abundance pipeline

---
+++ mindpipe/pipelines/src/otu_processing/filter/abundance/process.nf
@@ -0,0 +1,32 @@
+#!/usr/bin/env nextflow
+
+// Initialize variables
+def output_dir = file(params.output_dir)
+def otu_table = params.otu_table
+
+
+// Parameters
+def count_thres = params.count_thres
+def prevalence_thres = params.prevalence_thres
+def abundance_thres = params.abundance_thres
+
+
+// Channels
+Channel
+    .fromPath(otu_table)
+    .ifEmpty { exit 1, "Otu files not found" }
+    .map { tuple(it.baseName,  it) }
+    .set { chnl_otudata }
+
+
+// Processes
+process filter {
+    tag "${id}"
+    publishDir "${output_dir}/filter/abundance/${id}"
+    input:
+    set val(id), file(otu_file) from chnl_otudata
+    output:
+    set val(id), file("*_filtered.biom") into chnl_output
+    script:
+    {{ filter }}
+}
---
+++ mindpipe/pipelines/src/otu_processing/filter/abundance/processes/filter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Script that filters samples and observations based on abundance
+
+import os
+
+from mindpipe import Otu
+
+
+def main(otu_file, count_thres, prevalence_thres, abundance_thres):
+    otu_data = Otu.load_data(otu_file)
+    filtered_otu_data = otu_data.rm_sparse_obs(
+        prevalence_thres, abundance_thres
+    ).rm_sparse_samples(count_thres)
+    fname, _ = os.path.splitext(otu_file)
+    filtered_otu_data.write(f"{fname}_filtered", file_type="biom")
+
+
+if __name__ == "__main__":
+    OTU_FILE = "${otu_file}"
+    COUNT_THRES = int("${count_thres}")
+    PREVALENCE_THRES = float("${prevalence_thres}")
+    ABUNDANCE_THRES = float("${abundance_thres}")
+    main(OTU_FILE, COUNT_THRES, PREVALENCE_THRES, ABUNDANCE_THRES)
