156bb835f883959afe441a9dfc64e4cfd61d4267
Dileep Kishore <k.dileep1994@gmail.com>
2019-02-25 14:26:28 -0500

    Fix: Output file names in `sparcc` pipeline

--- mindpipe/pipelines/src/network_inference/correlation/sparcc/processes/sparcc.sh
+++ mindpipe/pipelines/src/network_inference/correlation/sparcc/processes/sparcc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 fastspar --iterations $iterations --yes \
     --otu_table $otu_file \
-    --correlation ${otu_file.baseName}_corr.tsv \
-    --covariance ${otu_file.baseName}_cov.tsv \
+    --correlation ${otu_file.baseName.split("_otu")[0]}_corr.tsv \
+    --covariance ${otu_file.baseName.split("_otu")[0]}_cov.tsv \
     --threads ${threads}
