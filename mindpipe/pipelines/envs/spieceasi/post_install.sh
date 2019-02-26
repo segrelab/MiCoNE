30910a331e44e05de8b3f12cf076430c689daea2
Dileep Kishore <k.dileep1994@gmail.com>
2019-02-26 18:30:53 -0500

    Add: Environment specification for `spieceasi` pipeline

---
+++ mindpipe/pipelines/envs/spieceasi/post_install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+source activate mindpipe-spieceasi
+
+export TAR=$(which tar)
+
+R -e 'library(devtools); install_github("zdk123/SpiecEasi")'
+
+conda install r-huge=1.2.7 --yes
