ba46e8ebf548a210aae6a7541d82fd4d3d5314ba
Dileep Kishore <k.dileep1994@gmail.com>
2019-03-28 09:58:01 -0400

    Fix: Issue of `demultiplex_illumina` wrongly renaming files

--- mindpipe/pipelines/src/otu_assignment/sequence_processing/demultiplex_illumina/processes/export_sequences.py
+++ mindpipe/pipelines/src/otu_assignment/sequence_processing/demultiplex_illumina/processes/export_sequences.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import csv
+import shutil
 import subprocess
 
 
@@ -13,7 +14,10 @@ def fix_manifest(manifest_file):
                 data.append(line.strip().split(","))
     header[1] = "absolute-filepath"
     for i in range(len(data)):
-        data[i][1] = "\$PWD/" + data[i][1]
+        fname = "demux_seqs/" + data[i][1]
+        new_fname = "demux_seqs/" + data[i][0] + ".fastq.gz"
+        shutil.move(fname, new_fname)
+        data[i][1] = "\$PWD/" + data[i][0] + ".fastq.gz"
     with open(manifest_file, "w") as fid:
         csv_writer = csv.writer(fid, delimiter=",")
         csv_writer.writerow(header)
