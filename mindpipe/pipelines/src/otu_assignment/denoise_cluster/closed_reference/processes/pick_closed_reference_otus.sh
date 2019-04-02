e6f325f12d87d4dc8036b5cf44a23d99f002734c
Dileep Kishore <k.dileep1994@gmail.com>
2019-04-02 11:40:06 -0400

    Change: `closed_reference` OTU ids are now sequence hashes

--- mindpipe/pipelines/src/otu_assignment/denoise_cluster/closed_reference/processes/pick_closed_reference_otus.sh
+++ mindpipe/pipelines/src/otu_assignment/denoise_cluster/closed_reference/processes/pick_closed_reference_otus.sh
@@ -12,3 +12,6 @@ pick_rep_set.py \
   -i uclust_ref_picked_otus/${id}_otus.txt \
   -f ${fasta_file} \
   -o rep_seqs.fasta
+
+mv otu_table.biom unhashed_otu_table.biom
+mv rep_seqs.fasta unhashed_rep_seqs.fasta
