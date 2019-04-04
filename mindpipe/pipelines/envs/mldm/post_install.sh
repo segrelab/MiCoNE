4bbf708fd9d1da19f05514b7741b73edb393cf9a
Dileep Kishore <k.dileep1994@gmail.com>
2019-04-04 08:26:58 -0400

    Add: Post install script for `mindpipe-mldm` en. Fixes #174

---
+++ mindpipe/pipelines/envs/mldm/post_install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+URL="https://github.com/tinglab/mLDM/raw/master/mLDM_1.1.tar.gz"
+FOLDER=/tmp/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+
+mkdir "$FOLDER"
+wget -O "$FOLDER/mldm.tar.gz" --quiet "$URL"
+export MLDM_ZIP="$FOLDER/mldm.tar.gz"
+
+source activate mindpipe-mldm
+
+R -e 'install.packages(c("lbfgs", "QUIC", "RcppEigen"), repos="http://archive.linux.duke.edu/cran/")'
+R -e 'install.packages(Sys.getenv("MLDM_ZIP"), repos=NULL, type="source")'
