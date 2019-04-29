d93187ae3ecb180019296325fbaf9ac801f1e5ac
Dileep Kishore <k.dileep1994@gmail.com>
2019-04-29 15:52:03 -0400

    Fix: Remove unused module from `tax_assignment` pipelines

--- mindpipe/pipelines/src/tax_assignment/assign/taxonomy/processes/taxonomy_assignment.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-qiime feature-classifier classify-sklearn \
-    --i-classifier ${classifier_artifact} \
-    --i-reads ${repseq_artifact} \
-    --p-n-jobs ${n_jobs} \
-    --p-reads-per-batch ${reads_per_batch} \
-    --o-classification ${repseq_artifact.baseName}_taxonomy.qza
