d9f70b404cbd5967e0b8a7bc06f3c59d6efb2765
Dileep Kishore <k.dileep1994@gmail.com>
2019-05-17 14:20:17 -0400

    Change: Replace `q2cli` with `Artifact API` in `trim_filter_fixed`

---
+++ mindpipe/pipelines/src/otu_assignment/sequence_processing/trim_filter_fixed/processes/export_visualization.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from qiime2 import Artifact
+from qiime2.plugins import demux
+
+
+def main(sequence_artifact, seq_samplesize, output_dir):
+    demux_artifact = Artifact.load(sequence_artifact)
+    demux_viz = demux.visualizers.summarize(demux_artifact, n=seq_samplesize)
+    demux_viz.export_data(output_dir)
+
+
+if __name__ == "__main__":
+    SEQUENCE_ARTIFACT = "${sequence_artifact}"
+    SEQ_SAMPLESIZE = "${seq_samplesize}"
+    OUTPUT_DIR = "output"
+    main(SEQUENCE_ARTIFACT, SEQ_SAMPLESIZE, OUTPUT_DIR)
