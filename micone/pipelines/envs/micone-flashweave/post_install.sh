b7985a822a3f4b1fe4c3f1b008d3dc6b05fe82de
Dileep Kishore <k.dileep1994@gmail.com>
2021-02-12 09:23:58 -0500

    Add: Create `conda` environment for `micone-flashweave`

---
+++ micone/pipelines/envs/micone-flashweave/post_install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env #!/bin/bash
+
+set -e
+
+MAIN_VERSION="1.5"
+VERSION="julia-1.5.3"
+FNAME="$MAIN_VERSION/$VERSION-linux-x86_64.tar.gz"
+
+URL="https://julialang-s3.julialang.org/bin/linux/x64/$FNAME"
+FOLDER=/tmp/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+
+mkdir "$FOLDER"
+wget -O "$FOLDER/julia.tar.gz" --quiet "$URL"
+tar xzf "$FOLDER/julia.tar.gz" -C "$FOLDER"
+
+source activate micone-flashweave
+
+cp -r "$FOLDER/$VERSION" "$CONDA_PREFIX/"
+ln -s "$CONDA_PREFIX/$VERSION/bin/julia" "$CONDA_PREFIX/bin/julia"
+
+source activate micone-flashweave
+julia -e 'using Pkg; Pkg.add("FlashWeave")'
