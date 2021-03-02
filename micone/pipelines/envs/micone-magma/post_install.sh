06c0ed4c2e03f0a09a3673c69ee644dfc5ee4d0f
Dileep Kishore <k.dileep1994@gmail.com>
2021-03-02 08:28:21 -0500

    Remove: MAGMA and SPRING pipelines

--- micone/pipelines/envs/micone-magma/post_install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-source activate micone-magma
-
-R -e 'library(devtools); install_gitlab("arcgl/rmagma")'
--- micone/pipelines/envs/micone-spring/post_install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-source activate micone-spring
-
-R -e 'library(devtools); install_github("GraceYoon/SPRING")'
