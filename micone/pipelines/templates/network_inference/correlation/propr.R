86a19b0f941d6383754e2b842ff20e5e262f24ec
Dileep Kishore <k.dileep1994@gmail.com>
2021-05-21 10:48:32 -0400

    Add: Config for propr workflow

--- micone/pipelines/templates/network_inference/correlation/propr.R
+++ micone/pipelines/templates/network_inference/correlation/propr.R
@@ -1,9 +1,13 @@
 #!/usr/bin/env Rscript
 
 library(propr)
+libarary(parallel)
 
+ncpus <- ${ncpus}
 otu_file <- "${otu_file}"
-corr_file <- "${otu_file.baseName.split('_otu')[0]}_corr.tsv"
+corr_file <- "${meta.id}_corr.tsv"
+
+# cl <- makeCluster(ncpus)
 
 read_otu <- function(otufile) {
     otu <- read.table(otufile, header=TRUE, comment.char="", sep="\\t")
@@ -12,12 +16,16 @@ read_otu <- function(otufile) {
     return(otu)
 }
 
+apply_propr <- function(otufile) {
+    otu <- read_otu(otu_file)
+    corr_file <- gsub("_otu.boot", "_corr.boot", otu_file)
+    pr <- propr(t(otu), metric = "rho", ivar = "clr")
+    mat <- pr@matrix
+    matdims <- rep(list(rownames(otu)), 2)
+    dimnames(mat) <- matdims
+    write.table(mat, file=corr_file, sep="\\t", quote=FALSE, col.names=NA)
+    return corr_file
+}
 
-otu <- read_otu(otu_file)
-
-pr <- propr(t(otu), metric = "rho", ivar = "clr")
-mat <- pr@matrix
-matdims <- rep(list(rownames(otu)), 2)
-dimnames(mat) <- matdims
-
-write.table(mat, file=corr_file, sep="\\t", quote=FALSE, col.names=NA)
+bootstrap_files = list.files(pattern="*_otu.boot")
+results <- mclapply(bootstrap_files, apply_propr, mc.cores = ncpus)
