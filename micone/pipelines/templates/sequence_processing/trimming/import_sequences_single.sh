115a7ae126a9cda1f33ae5686ef52ad1eeb13ce4
Dileep Kishore <k.dileep1994@gmail.com>
2021-11-05 14:32:23 -0400

    Fix: Bug in trimming module for single end

--- micone/pipelines/templates/sequence_processing/trimming/import_sequences_single.sh
+++ micone/pipelines/templates/sequence_processing/trimming/import_sequences_single.sh
@@ -5,7 +5,7 @@ cp "${manifest_file}" "${meta.id}"/
 cp "${sequence_metadata}" "${meta.id}"/
 cp *.fastq.gz "${meta.id}"/
 
-qiime tools export \
+qiime tools import \
     --input-path "${meta.id}" \
     --output-path "${meta.id}_sequences.qza" \
     --type "SampleData[SequencesWithQuality]"
