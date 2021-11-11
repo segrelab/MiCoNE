e39be7d85d995e62d0b49741f5e3bbc4d8b0a66f
Dileep Kishore <k.dileep1994@gmail.com>
2021-11-11 08:53:31 -0500

    Fix: Transpose sample_metadata after reading into DataFrame
    Earlier version has `transpose=true` in the `CSV.read` function but this
    tends to segfault when the column names are long. Hence, we have
    transitioned into transposing the `DataFrame` independently and then
    write it

--- micone/pipelines/templates/network_inference/direct/flashweave.jl
+++ micone/pipelines/templates/network_inference/direct/flashweave.jl
@@ -7,6 +7,7 @@ addprocs(${ncpus})
 
 using CSV
 using DataFrames
+metadata_path = "fmt-control-Family_sample_metadata.tsv"
 
 # data
 data_path = "${otu_file}"
@@ -15,9 +16,13 @@ if data_path != new_data_path
     cp(data_path, new_data_path, force=true, follow_symlinks=true)
 end
 metadata_path = "${sample_metadata}"
-metadata = CSV.read(metadata_path, DataFrame; transpose=true)
+# NOTE: Transposing directly here does not work if the column names are long
+metadata = CSV.read(metadata_path, DataFrame)
+metadata_transpose = DataFrame(
+    [[names(metadata)]; collect.(eachrow(metadata))], [:column; Symbol.(axes(metadata, 1))]
+)
 new_metadata_path = "${sample_metadata.baseName}_transposed.tsv"
-CSV.write(new_metadata_path, metadata; delim="\\t")
+CSV.write(new_metadata_path, metadata_transpose; writeheader=false, delim="\\t")
 
 # options
 sensitive = ${sensitive}
