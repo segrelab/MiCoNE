518c3af68203ec52bee4669547fddab2fa88a4a5
Dileep Kishore <k.dileep1994@gmail.com>
2021-11-15 17:28:02 -0500

    Add: HARMONIES environment for NI

---
+++ micone/pipelines/envs/micone-harmonies/post_install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+source activate micone-harmonies
+
+R -e 'install.packages(c("mvnfast", "networkD3"), repo="https://mirror.las.iastate.edu/CRAN/")'
+R -e 'library(devtools); install_github("shuangj00/HARMONIES", subdir = "pkg" )'
