3bd00199ed66f6f8c6e169c0bd05f8db6d849ff8
Dileep Kishore <k.dileep1994@gmail.com>
2021-11-15 17:30:39 -0500

    Remove: NetCoMi environment

--- micone/pipelines/envs/micone-netcomi/post_install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-source activate micone-netcomi
-
-R -e 'devtools::install_github("stefpeschel/NetCoMi", dependencies = TRUE, repos = c("https://cloud.r-project.org/", BiocManager::repositories())); NetCoMi::installNetCoMiPacks()'
